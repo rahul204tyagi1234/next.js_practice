7f8ed13506efc7c7db735c30b232ec9f45afd5cc
Rahul Tyagi <rahul204tyagi1234@gmail.com>
2023-12-22 21:11:38 +0530

    my today work

---
+++ add-css/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Style and CSS with Next.js</h1>
+      <h2>Heading 2 in main page</h2>
+    </main>
+  )
+}
--- fatch-client/src/app/productlist/page.tsx
+++ fatch-client/src/app/productlist/page.tsx
@@ -1,7 +1,21 @@
-export default function ProductList(){
-    return(
-        <div>
-            <h1>Product List</h1>
-        </div>
-    )
+"use client";
+import { useEffect, useState } from "react";
+
+export default function ProductList() {
+  const [product, setProduct] = useState([]);
+  useEffect(async () => {
+      let data = await fetch("https://dummyjson.com/products");
+      data =  await data.json();
+
+      setProduct(data.products);
+  }, []);
+  return (
+    <div>
+      <h1>Product List</h1>
+      {
+        product.map((item) => (
+        <h3>Name:{item.title}</h3>
+      ))}
+    </div>
+  );
 }
